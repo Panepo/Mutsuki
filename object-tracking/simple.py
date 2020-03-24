b489d61340612d92ee87e58c6092482f03a81f43
Panepo <whisper.ko@gmail.com>
2020-03-24 13:49:54 +0800

    add sub project
    add object tracking

---
+++ object-tracking/simple.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+tracker = cv2.Tracker_create("KCF")
+traceStart = 0
+
+while(True):
+    (grabbed, img) = cap.read()
+    if not grabbed:
+        break
+
+    if traceStart:
+        ok, bbox = tracker.update(img)
+
+        if ok:
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(img, p1, p2, (0,255,0), 3)
+
+    cv2.imshow("Tracking", img)
+
+    getKey = cv2.waitKey(20) & 0xFF
+    if getKey == ord('q'):
+        break
+    elif getKey == ord('c'):
+        bbox = cv2.selectROI(img, False)
+        ok = tracker.init(img, bbox)
+        traceStart = 1
+        cv2.destroyAllWindows()
+
+cap.release()
+cv2.destroyAllWindows()
