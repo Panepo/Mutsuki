de6d266a3f3bf8cbe5137d0583b071cced9538bf
Panepo <whisper.ko@gmail.com>
2020-03-25 11:39:58 +0800

    update code
    update simple detector and tracker

--- object-tracking/simpleDetector.py
+++ object-tracking/simpleDetector.py
@@ -1,72 +1,188 @@
+import logging as log
+import sys
+from argparse import ArgumentParser
 import cv2
 import numpy as np
 import time
 
-pathWeight = './models/yolov3-tiny.weights'
-pathConfig = './models/yolov3-tiny.cfg'
+BREAK_KEYS = {ord("q"), ord("Q"), 27}
+CAPTURE_KEYS = {ord("c"), ord("C")}
 
-pathLabel = './models/coco.names'
-LABELS = open(pathLabel).read().strip().split("\n")
-np.random.seed(42)
-COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),dtype="uint8")
+def build_argparser():
+    parser = ArgumentParser()
 
-print("[INFO] loading YOLO from disk...")
-net = cv2.dnn.readNetFromDarknet(pathConfig, pathWeight)
+    general = parser.add_argument_group("General")
+    general.add_argument(
+        "-i",
+        "--input",
+        metavar="PATH",
+        default="0",
+        help="(optional) Path to the input video " "('0' for the camera, default)",
+    )
+    general.add_argument(
+        "-tl",
+        "--timelapse",
+        action="store_true",
+        help="(optional) Auto-pause after each frame",
+    )
+    general.add_argument(
+        "-cw",
+        "--crop_width",
+        default=0,
+        type=int,
+        help="(optional) Crop the input stream to this width "
+        "(default: no crop). Both -cw and -ch parameters "
+        "should be specified to use crop.",
+    )
+    general.add_argument(
+        "-ch",
+        "--crop_height",
+        default=0,
+        type=int,
+        help="(optional) Crop the input stream to this height "
+        "(default: no crop). Both -cw and -ch parameters "
+        "should be specified to use crop.",
+    )
 
-cap = cv2.VideoCapture(0)
+    detections = parser.add_argument_group("Detections")
+    detections.add_argument(
+        "-pw",
+        "--path_weight",
+        metavar="PATH",
+        default='./models/yolov3-tiny.weights',
+        help="Path to YOLO weights",
+    )
+    detections.add_argument(
+        "-pc",
+        "--path_config",
+        metavar="PATH",
+        default='./models/yolov3-tiny.cfg',
+        help="Path to YOLO configs",
+    )
+    detections.add_argument(
+        "-pl",
+        "--path_label",
+        metavar="PATH",
+        default='./models/coco.names',
+        help="Path to YOLO labels",
+    )
 
-while(True):
-    (grabbed, image) = cap.read()
-    if not grabbed:
-        break
+    return parser
 
-    (H, W) = image.shape[:2]
-
-    ln = net.getLayerNames()
-    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
-    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),swapRB=True, crop=False)
-    net.setInput(blob)
-    start = time.time()
-    layerOutputs = net.forward(ln)
-    end = time.time()
-    print("[INFO] YOLO took {:.6f} seconds".format(end - start))
-
-    boxes = []
-    confidences = []
-    classIDs = []
-
-    for output in layerOutputs:
-        for detection in output:
-            scores = detection[5:]
-            classID = np.argmax(scores)
-            confidence = scores[classID]
-            if confidence > 0.5:
-                box = detection[0:4] * np.array([W, H, W, H])
-                (centerX, centerY, width, height) = box.astype("int")
-                x = int(centerX - (width / 2))
-                y = int(centerY - (height / 2))
-                boxes.append([x, y, int(width), int(height)])
-                confidences.append(float(confidence))
-                classIDs.append(classID)
-
-    idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
-
-    if len(idxs) > 0:
-        for i in idxs.flatten():
-            (x, y) = (boxes[i][0], boxes[i][1])
-            (w, h) = (boxes[i][2], boxes[i][3])
-            color = [int(c) for c in COLORS[classIDs[i]]]
-            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-            text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
-            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,0.5, color, 2)
+def center_crop(frame, crop_size):
+    fh, fw, fc = frame.shape
+    crop_size[0] = min(fw, crop_size[0])
+    crop_size[1] = min(fh, crop_size[1])
+    return frame[
+        (fh - crop_size[1]) // 2 : (fh + crop_size[1]) // 2,
+        (fw - crop_size[0]) // 2 : (fw + crop_size[0]) // 2,
+        :,
+    ]
 
 
-    cv2.imshow("Detector", image)
+def save_result(image, name):
+    fileName = (
+        "./output/"
+        + name
+        + "_"
+        + time.strftime("%Y-%m-%d_%H%M%S-", time.localtime())
+        + ".png"
+    )
+    cv2.imwrite(fileName, image, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+    log.info("saved results to {}".format(fileName))
 
-    getKey = cv2.waitKey(20) & 0xFF
-    if getKey == ord('q'):
-        break
+def main():
+    args = build_argparser().parse_args()
 
-cap.release()
-cv2.destroyAllWindows()
+    log.basicConfig(
+        format="[ %(levelname)s ] %(asctime)-15s %(message)s",
+        level=log.INFO,
+        stream=sys.stdout,
+    )
+    log.debug(str(args))
+
+    cap = cv2.VideoCapture(args.input)
+    frame_size = (
+        int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+    )
+
+    input_crop = None
+    if args.crop_width and args.crop_height:
+        input_crop = np.array((args.crop_width, args.crop_height))
+        crop_size = (args.crop_width, args.crop_height)
+        frame_size = tuple(np.minimum(frame_size, crop_size))
+
+    frame_timeout = 0 if args.timelapse else 1
+
+    log.info("loading YOLO from disk...")
+    net = cv2.dnn.readNetFromDarknet(args.path_config, args.path_weight)
+    LABELS = open(args.path_label).read().strip().split("\n")
+    np.random.seed(42)
+    COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),dtype="uint8")
+
+    while(True):
+        (grabbed, frame) = cap.read()
+        if not grabbed:
+            break
+
+        start = time.time()
+
+        if input_crop is not None:
+            frame = center_crop(frame, input_crop)
+
+        (H, W) = frame.shape[:2]
+
+        ln = net.getLayerNames()
+        ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),swapRB=True, crop=False)
+        net.setInput(blob)
+        layerOutputs = net.forward(ln)
+
+        boxes = []
+        confidences = []
+        classIDs = []
+
+        for output in layerOutputs:
+            for detection in output:
+                scores = detection[5:]
+                classID = np.argmax(scores)
+                confidence = scores[classID]
+                if confidence > 0.5:
+                    box = detection[0:4] * np.array([W, H, W, H])
+                    (centerX, centerY, width, height) = box.astype("int")
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
+                    boxes.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))
+                    classIDs.append(classID)
+
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
+
+        if len(idxs) > 0:
+            for i in idxs.flatten():
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+                color = [int(c) for c in COLORS[classIDs[i]]]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+                cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,0.5, color, 2)
+
+        cv2.imshow("Detector", frame)
+
+        end = time.time()
+        log.info("Detecting took {:.6f} seconds".format(end - start))
+
+        getKey = cv2.waitKey(frame_timeout) & 0xFF
+        if getKey in BREAK_KEYS:
+            break
+        elif getKey in CAPTURE_KEYS:
+            log.info("Screen captured")
+            save_result(frame, "tracking")
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
--- object-tracking/simpleTracker.py
+++ object-tracking/simpleTracker.py
@@ -1,33 +1,164 @@
+import logging as log
+import sys
+from argparse import ArgumentParser
 import cv2
 import numpy as np
+import time
 
-cap = cv2.VideoCapture(0)
-tracker = cv2.TrackerKCF_create()
-traceStart = 0
+TRACKER_KINDS = ["KCF", "BOOSTING", "MIL", "TLD", "MEDIANFLOW", "GOTURN"]
 
-while(True):
-    (grabbed, img) = cap.read()
-    if not grabbed:
-        break
+BREAK_KEYS = {ord("q"), ord("Q"), 27}
+CAPTURE_KEYS = {ord("c"), ord("C")}
+TRACKING_KEYS = {ord("t"), ord("T")}
 
-    if traceStart:
-        ok, bbox = tracker.update(img)
+def build_argparser():
+    parser = ArgumentParser()
 
-        if ok:
-            p1 = (int(bbox[0]), int(bbox[1]))
-            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            cv2.rectangle(img, p1, p2, (0,255,0), 3)
+    general = parser.add_argument_group("General")
+    general.add_argument(
+        "-i",
+        "--input",
+        metavar="PATH",
+        default="0",
+        help="(optional) Path to the input video " "('0' for the camera, default)",
+    )
+    general.add_argument(
+        "-tl",
+        "--timelapse",
+        action="store_true",
+        help="(optional) Auto-pause after each frame",
+    )
+    general.add_argument(
+        "-cw",
+        "--crop_width",
+        default=0,
+        type=int,
+        help="(optional) Crop the input stream to this width "
+        "(default: no crop). Both -cw and -ch parameters "
+        "should be specified to use crop.",
+    )
+    general.add_argument(
+        "-ch",
+        "--crop_height",
+        default=0,
+        type=int,
+        help="(optional) Crop the input stream to this height "
+        "(default: no crop). Both -cw and -ch parameters "
+        "should be specified to use crop.",
+    )
 
-    cv2.imshow("Tracking", img)
+    detections = parser.add_argument_group("Detections")
+    detections.add_argument(
+        "-t",
+        "--tracker",
+        type=str,
+        choices=TRACKER_KINDS,
+        default="KCF",
+        help="OpenCV object tracker type",
+    )
 
-    getKey = cv2.waitKey(20) & 0xFF
-    if getKey == ord('q'):
-        break
-    elif getKey == ord('c'):
-        bbox = cv2.selectROI(img, False)
-        ok = tracker.init(img, bbox)
-        traceStart = 1
-        cv2.destroyAllWindows()
+    return parser
 
-cap.release()
-cv2.destroyAllWindows()
+def center_crop(frame, crop_size):
+    fh, fw, fc = frame.shape
+    crop_size[0] = min(fw, crop_size[0])
+    crop_size[1] = min(fh, crop_size[1])
+    return frame[
+        (fh - crop_size[1]) // 2 : (fh + crop_size[1]) // 2,
+        (fw - crop_size[0]) // 2 : (fw + crop_size[0]) // 2,
+        :,
+    ]
+
+
+def save_result(image, name):
+    fileName = (
+        "./output/"
+        + name
+        + "_"
+        + time.strftime("%Y-%m-%d_%H%M%S-", time.localtime())
+        + ".png"
+    )
+    cv2.imwrite(fileName, image, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+    log.info("saved results to {}".format(fileName))
+
+
+def main():
+    args = build_argparser().parse_args()
+
+    log.basicConfig(
+        format="[ %(levelname)s ] %(asctime)-15s %(message)s",
+        level=log.INFO,
+        stream=sys.stdout,
+    )
+    log.debug(str(args))
+
+    cap = cv2.VideoCapture(args.input)
+    frame_size = (
+        int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+    )
+
+    input_crop = None
+    if args.crop_width and args.crop_height:
+        input_crop = np.array((args.crop_width, args.crop_height))
+        crop_size = (args.crop_width, args.crop_height)
+        frame_size = tuple(np.minimum(frame_size, crop_size))
+
+    frame_timeout = 0 if args.timelapse else 1
+
+    if args.tracker == 'BOOSTING':
+        tracker = cv2.TrackerBoosting_create()
+    elif args.tracker == 'MIL':
+        tracker = cv2.TrackerMIL_create()
+    elif args.tracker == 'KCF':
+        tracker = cv2.TrackerKCF_create()
+    elif args.tracker == 'TLD':
+        tracker = cv2.TrackerTLD_create()
+    elif args.tracker == 'MEDIANFLOW':
+        tracker = cv2.TrackerMedianFlow_create()
+    elif args.tracker == 'GOTURN':
+        tracker = cv2.TrackerGOTURN_create()
+
+    traceStart = 0
+
+    while(True):
+        (grabbed, frame) = cap.read()
+        if not grabbed:
+            break
+
+        start = time.time()
+
+        if input_crop is not None:
+            frame = center_crop(frame, input_crop)
+
+        if traceStart:
+            ok, bbox = tracker.update(frame)
+
+            if ok:
+                p1 = (int(bbox[0]), int(bbox[1]))
+                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+                cv2.rectangle(frame, p1, p2, (0,255,0), 3)
+
+            end = time.time()
+            log.info("Tracking took {:.6f} seconds".format(end - start))
+
+        cv2.imshow("Tracking", frame)
+
+        getKey = cv2.waitKey(frame_timeout) & 0xFF
+        if getKey in BREAK_KEYS:
+            break
+        elif getKey in TRACKING_KEYS:
+            bbox = cv2.selectROI(frame, False)
+            ok = tracker.init(frame, bbox)
+            traceStart = 1
+            cv2.destroyAllWindows()
+        elif getKey in CAPTURE_KEYS:
+            log.info("Screen captured")
+            save_result(frame, "tracking")
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
